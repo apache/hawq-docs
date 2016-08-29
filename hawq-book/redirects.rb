52ce16bfb368ef3fc43782fe22f4ad0c85749842
David Yozie <yozie@apache.org>
2016-08-29 10:18:49 -0700

    removing hawq-book directory from this branch [HAWQ-1027]

--- hawq-book/redirects.rb
+++
@@ -1,6 +0,0 @@
-r301 %r{.*}, 'http://docs-hdb-staging$&', if: Proc.new {|rack_env|
-  rack_env['SERVER_NAME'] == 'docs-hawq-staging'
-}
-rewrite '/', '/20/overview/HAWQOverview.html'
-r301 '/index.html', '/20/overview/HAWQOverview.html'
-
