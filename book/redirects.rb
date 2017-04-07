975ef85d8c4725a313fee8488c0c3d0372d16ce4
David Yozie <yozie@apache.org>
2017-04-07 09:16:47 -0700

    updating redirects for 2.2.0.0-incubating

--- book/redirects.rb
+++ book/redirects.rb
@@ -1,4 +1,4 @@
-r301 '/', '/docs/userguide/2.1.0.0-incubating/overview/HAWQOverview.html'
-r301 '/index.html', '/docs/userguide/2.1.0.0-incubating/overview/HAWQOverview.html'
-r301 '/docs', '/docs/userguide/2.1.0.0-incubating/overview/HAWQOverview.html'
+r301 '/', '/docs/userguide/2.2.0.0-incubating/overview/HAWQOverview.html'
+r301 '/index.html', '/docs/userguide/2.2.0.0-incubating/overview/HAWQOverview.html'
+r301 '/docs', '/docs/userguide/2.2.0.0-incubating/overview/HAWQOverview.html'
 
