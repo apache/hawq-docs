3c188fdf1251cac9d5d3bbec6052561bf39e8ab9
David Yozie <yozie@apache.org>
2018-03-20 17:02:52 -0700

    updating book build for 2.3 release

--- book/redirects.rb
+++ book/redirects.rb
@@ -1,4 +1,4 @@
-r301 '/', '/docs/userguide/2.2.0.0-incubating/overview/HAWQOverview.html'
-r301 '/index.html', '/docs/userguide/2.2.0.0-incubating/overview/HAWQOverview.html'
-r301 '/docs', '/docs/userguide/2.2.0.0-incubating/overview/HAWQOverview.html'
+r301 '/', '/docs/userguide/2.3.0.0-incubating/overview/HAWQOverview.html'
+r301 '/index.html', '/docs/userguide/2.3.0.0-incubating/overview/HAWQOverview.html'
+r301 '/docs', '/docs/userguide/2.3.0.0-incubating/overview/HAWQOverview.html'
 
